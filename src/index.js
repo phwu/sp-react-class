62ebf95dbc2b360614d6564bd0b444db4707ce44
John Paxton <pax@speedingplanet.com>
2017-06-04 15:01:01 -0400

    Reverting to proper initial state.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './App';
-
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import '../node_modules/bootstrap/dist/css/bootstrap-theme.css';
-
-ReactDOM.render(
-  <App/>,
-  document.getElementById( 'root' )
-);
+// Placeholder
