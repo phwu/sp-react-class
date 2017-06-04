3d801a5081a8fc5ccd53b902db33ccc616bb52ee
John Paxton <pax@speedingplanet.com>
2017-06-04 15:19:58 -0400

    Exercise 2 solution

--- exercises/ex-02/solution/src/App.spec.js
+++ exercises/ex-02/solution/src/App.spec.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {shallow} from 'enzyme';
 
-test( 'Adds 2 and 2 to equal 4', () => {
-  expect( 2 + 2 ).toBe( 4 );
-} );
-
-test('renders without crashing', () => {
+it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+test( 'Adds 2 and 2 to equal 4', () => {
+  expect( 2 + 2 ).toBe( 4 );
+} );
 
 test('Contains "Hello"', () => {
   const wrapper = shallow(<App/>);
--- exercises/ex-02/solution/src/AppClass.js
+++
@@ -1,9 +0,0 @@
-import {Component} from 'react';
-
-export default class AppClass extends Component {
-  render() {
-    return (
-      <h1>Hello, world!</h1>
-    )
-  }
-}
